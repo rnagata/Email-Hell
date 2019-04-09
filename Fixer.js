1d00d85f9dbbcff79bb28c84aef44053f247f4d5
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-09 09:45:19 -1000

    First Solution

--- Fixer.js
+++ Fixer.js
@@ -1,1 +1,32 @@
+const json = require('./email-log.json');
 
+const fixer = function () {
+
+  function uniqueEmailCount(inputArr){
+    let results = [];
+    inputArr.forEach( function (inputElem){
+      let index = results.findIndex( function (resultsElem){
+        return resultsElem.address === inputElem.email;
+      });
+
+      if (index === -1){
+        results.push({
+          address : inputElem.email,
+          count : 1
+        })
+        return;
+      }
+
+      results[index].count++;
+    });
+    
+    return results;
+  }
+
+  return {
+    uniqueEmailCount,
+  }
+
+}();
+
+console.log(fixer.uniqueEmailCount(json.emails));
